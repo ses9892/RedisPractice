b01671fa4323c8964e735de823a9a2b1a2eb1286
jinho <ses1238@gmail.com>
2024-12-05 16:45:52 +0900

    Gradle 설정 Redis 의존성 추가

--- RedisExample/build.gradle.kts
+++ RedisExample/build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("redis.clients:jedis:4.6.3")
 }
 
 tasks.test {
